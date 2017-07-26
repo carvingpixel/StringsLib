68d042b2b95d665a11ae950f623c8ae54fb9cca9
tk429 <tk429@DESKTOP-074LP29>
2017-07-26 10:42:35 -0400

    Add reload assembly to console test

--- StringsTest/Program.cs
+++ StringsTest/Program.cs
@@ -3,8 +3,11 @@ using StringsLib;
 
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
 
 namespace StringsTest
 {
@@ -19,29 +22,33 @@ namespace StringsTest
             //accept a string from user as input TEST set to lower
             Console.WriteLine("Provide a string quote to check");
             string givenQuote = Console.ReadLine();
-            givenQuote.ToLower();
+            string lowerQuote = givenQuote.ToLower();
 
 
             //Analyze total count of words
-            var wordCount = checkString.CountTotal(givenQuote);
-            Console.WriteLine("Total Word Count: " + wordCount);
+            Console.WriteLine("Total Word Count: " + checkString.CountTotal(lowerQuote));
 
 
             //Total Count of Unique Words 
-            checkString.WordsUnique(givenQuote);
+            Console.WriteLine("Unique Words:" + checkString.WordsUnique(lowerQuote));
 
 
-            //Uunique words  
-            Dictionary<string, int> myDictionary = checkString.CountUnique(givenQuote);
+            //Count Each Unique Word and display 
+            Dictionary<string, int> myDictionary = checkString.CountUnique(lowerQuote);
             foreach (var subItem in myDictionary)
             {
-               Console.WriteLine(subItem.Key + " " + subItem.Value);
+                Console.WriteLine(subItem.Key + " " + subItem.Value);
             }
 
-
-
+            Console.Write("Type R to Reload Check: ");
             // All items complete in Analysis Library
-            Console.ReadLine();
+
+            var reload = Console.ReadKey();
+            if (reload.Key == ConsoleKey.R)
+            {
+                var SimpleCheck = Assembly.GetExecutingAssembly().Location;
+                System.Diagnostics.Process.Start(SimpleCheck);
+            }
 
         }
     }
